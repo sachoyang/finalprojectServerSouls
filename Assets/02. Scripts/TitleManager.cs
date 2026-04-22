3c9b863690f6b04ccfb681eb33a6432e5baae9d4
DESKTOP-1SE37VB\user <qhsljywn@gmail.com>
2026-04-22 16:38:35 +0900

    임시 UI리소스

--- Assets/02. Scripts/TitleManager.cs
+++ Assets/02. Scripts/TitleManager.cs
@@ -6,7 +6,7 @@ public class TitleManager : MonoBehaviour
     public GameObject tutorialPopupPanel;
     public GameObject mainTitlePanel;
     public GameObject matchPanel; // 새로 만든 매칭 패널
-
+    public GameObject CreatePanel;
     void Start()
     {
         CheckTutorialStatus();
@@ -58,7 +58,20 @@ public class TitleManager : MonoBehaviour
     // 시작(Play) 버튼을 눌렀을 때 실행될 함수
     public void OnClickPlayButton()
     {
-        mainTitlePanel.SetActive(false); // 메인 타이틀 화면 끄기
+        mainTitlePanel.SetActive(true); // 메인 타이틀 화면 끄기
         matchPanel.SetActive(true);      // 매칭 패널 켜기
     }
+
+    public void OnClickEnterButton()
+    {
+        matchPanel.SetActive(false); // 매칭 패널 끄기
+        CreatePanel.SetActive(true);      // 생성 패널 켜기
+    }
+
+    public void OnClickCloseButton()
+    {
+        matchPanel.SetActive(false); // 매칭 패널 끄기
+        CreatePanel.SetActive(false);      // 생성 패널 켜기
+    }
+
 }
